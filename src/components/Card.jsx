4aeb46f86593a01846870f2e8a500f96235c9d9a
matejadb <92729211+matejadb@users.noreply.github.com>
2025-05-19 18:15:25 +0200

    Styling tweaks
    Adjusted Pokemon names and put border radius on the cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,12 @@
 export default function Card({ name, imageLink }) {
+	name = name.split('');
+	name = name[0].toUpperCase() + name.slice(1).join('');
+	const formatName = name;
+
 	return (
 		<div className="card">
 			<img src={imageLink} alt={name + '-avatar'} />
-			<h2>{name}</h2>
+			<h2>{formatName}</h2>
 		</div>
 	);
 }
